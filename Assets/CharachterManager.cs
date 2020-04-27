bdaf450b04fc31fe27dfde47fb09fb8bf5cb4495
Madhav <madhavkhanna2021@u.northwestern.edu>
2020-04-27 00:09:31 -0500

    Update CharachterManager.cs

--- Assets/CharachterManager.cs
+++ Assets/CharachterManager.cs
@@ -7,5 +7,9 @@ public class CharachterManager : MonoBehaviour
     public StoryletMotivation currentMotivation;
     public StoryletMotivation endMotivation;
 
+    [Header("Motivation Variables")]
+    public int happiness;
+    public int excited;
     
+ 
 }
